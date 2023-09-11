aa374ba1c308d40c9d4caed2d552b2c4573ead8c
Josefina Husso <husso.josefina@gmail.com>
2023-09-11 08:17:49 +0300

    changinh branch

--- src/map_validation/wall_check.c
+++ src/map_validation/wall_check.c
@@ -6,7 +6,7 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 07:51:46 by jhusso            #+#    #+#             */
-/*   Updated: 2023/09/11 08:09:03 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/09/11 08:15:56 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 
 void	set_buffer(t_data *data)
 {
-	map_cpy = ft_calloc()
+	map_cpy = ft_calloc(data->height + 2, sizeof(char*));
 }
 
 int	wall_check(t_data *data)
