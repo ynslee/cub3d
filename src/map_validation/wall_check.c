c0e092c91f345987cf42876a48659c630bcb7c91
Josefina Husso <husso.josefina@gmail.com>
2023-09-11 12:21:41 +0300

    started floodfill

--- src/map_validation/wall_check.c
+++ src/map_validation/wall_check.c
@@ -6,7 +6,7 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 07:51:46 by jhusso            #+#    #+#             */
-/*   Updated: 2023/09/11 11:52:44 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/09/11 12:19:08 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,34 @@
 - flood fill to check walls are closed
 
 */
+
+// void	flood_fill_inside(char **map, t_data *mv)
+// {
+// 	int	x;
+// 	int	y;
+
+// 	x = mv->player_x;
+// 	y = mv->player_y;
+// 	if (x < 0 || y < 0 || x > mv->height - 1 || y > mv->width - 1 \
+// 		|| !mv->map_cpy[x][y])
+// 		return ;
+// 	else if (x >= 0 && y >= 0 && x < mv->height && y < mv->width \
+// 	&& mv->map_cpy[x][y])
+// 	{
+// 		if (mv->map_cpy[x][y] == '0' || mv->map_cpy[x][y] == mv->player)
+// 		{
+// 			mv->map_cpy[x][y] = x;
+// 			if (x < mv->height - 1)
+// 				flood_fill(mv, x + 1, y, c);
+// 			if (y < mv->width - 1)
+// 				flood_fill(mv, x, y + 1, c);
+// 			if (x > 0)
+// 				flood_fill(mv, x - 1, y, c);
+// 			if (y > 0)
+// 				flood_fill(mv, x, y - 1, c);
+// 		}
+// 	}
+// }
 
 static void	ft_bx(void *s, size_t n)
 {
@@ -55,6 +83,7 @@ void	set_buffer(t_data *data)
 	ft_bx(buffer_cpy[i], data->width + 2);
 	print_map(buffer_cpy);
 	free_char_array(buffer_cpy);
+	flood_fill_inside(buffer_cpy, data);
 }
 
 int	wall_check(t_data *data)
