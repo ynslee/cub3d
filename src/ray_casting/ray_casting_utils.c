1559f39d6676ac3310602a646d0fceee99490d60
chuchilini <vanesalees@gmail.com>
2023-09-13 13:38:00 +0300

    I added tiny thingy

--- src/ray_casting/ray_casting_utils.c
+++ src/ray_casting/ray_casting_utils.c
@@ -3,19 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ray_casting_utils.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 09:47:29 by jhusso            #+#    #+#             */
-/*   Updated: 2023/09/13 10:26:15 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/09/13 13:37:51 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/ray_casting.h"
 
-float	deg_to_rad(float a)
+float	deg_to_rad(float degree)
 {
-	return (a * M_PI / 180.0);
+	float	radians;
+
+	radians = degree * (M_PI / 180.0);
+	return (radians);
 }
+
 void	player_orientation_to_angle(t_data *mv, t_ray *ray)
 {
 	if (mv->player_view == 'N')
@@ -27,4 +31,6 @@ void	player_orientation_to_angle(t_data *mv, t_ray *ray)
 	if (mv->player_view == 'E')
 		ray->pa = 0.0;
 	printf("player angle = %f\n", ray->pa);
+	ray->cbd->x_angle = cos(deg_to_rad(ray->pa));
+	ray->cbd->y_angle = -sin(deg_to_rad(ray->pa));
 }
