e2597ed1bb8307ea752f2d25e0cc0903ce2a6fb1
chuchilini <vanesalees@gmail.com>
2023-10-04 15:00:03 +0300

    fixed ray that enters through between the walls

--- src/ray_casting/ray_casting.c
+++ src/ray_casting/ray_casting.c
@@ -6,11 +6,22 @@
 /*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 07:25:36 by jhusso            #+#    #+#             */
-/*   Updated: 2023/10/04 14:44:19 by yoonslee         ###   ########.fr       */
+/*   Updated: 2023/10/04 14:59:43 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/ray_casting.h"
+
+static int	is_wall(t_ray *ray, float x, float y)
+{
+	if ((ray->data->map[(int)(y / GRID_PIX - 0.01)][(int)x / GRID_PIX] == '1') || \
+	(ray->data->map[(int)(y / GRID_PIX + 0.01)][(int)x / GRID_PIX] == '1') || \
+	(ray->data->map[(int)y / GRID_PIX][(int)(x / GRID_PIX - 0.01)] == '1') || \
+	(ray->data->map[(int)y / GRID_PIX][(int)(x / GRID_PIX + 0.01)] == '1'))
+		return (1);
+	return (0);
+}
+
 
 void	cast_rays(t_ray *ray)
 {
@@ -26,15 +37,16 @@ void	cast_rays(t_ray *ray)
 		ray->pdy = -sin(deg_to_rad(ray->ra));
 		x += ray->pdx;
 		y += ray->pdy;
-		if (ray->data->map[(int)y / GRID_PIX][(int)x / GRID_PIX] == '1')
+		if (ray->data->map[(int)y / GRID_PIX][(int)x / GRID_PIX] == '1' \
+		|| is_wall(ray, x, y))
 		{
 			ray->r_end_x = x;
 			ray->r_end_y = y;
 			bresenham(ray);
-			ray->ray_count += 0.5;
-			ray->ra = fix_angle(ray->ra + 0.5);
+			ray->ray_count += 0.1;
+			ray->ra = fix_angle(ray->ra + 0.1);
 			cast_rays(ray);
-			break;
-	}
+			break ;
+		}
 	}
 }
