148fa44237b920950a8bb4917173df851b1094fd
chuchilini <vanesalees@gmail.com>
2023-11-02 14:53:30 +0200

    eating chicken added to cub3d

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:40:08 by jhusso            #+#    #+#             */
-/*   Updated: 2023/11/02 13:59:07 by yoonslee         ###   ########.fr       */
+/*   Updated: 2023/11/02 14:52:45 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@
 # include "ray_casting_bonus.h"
 # include "../libft/libft.h"
 
-# define WIN_SIZE_X 1000
-# define WIN_SIZE_Y 800
+# define WIN_SIZE_X 1920
+# define WIN_SIZE_Y 1080
 
 # define NO 0
 # define SO 1
